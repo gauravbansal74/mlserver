fbbdffd41cf76c66d18c89271d4b3e339fb85bfd
Gaurav Kumar <gauravbansal74@gmail.com>
2018-02-22 23:06:25 +0800

    changes for gitignore

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 go clean
-go test github.com/gauravbansal74/mlserver/pkg/utils
-go test github.com/gauravbansal74/mlserver/pkg/response
-go test github.com/gauravbansal74/mlserver/pkg/jwt
-go test github.com/gauravbansal74/mlserver/pkg/logger
-go test github.com/gauravbansal74/mlserver/server/exclusion
-
 go build
 ./mlserver --config=./.mlserver.yml server 
